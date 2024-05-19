8d41916ad08827c2653de74ae20b7d7383345847
Itai Hanski <itaihanski@gmail.com>
2024-05-19 16:51:32 +0300

    Support cookies via new core hook (#15)

--- src/internal/core/sdk.ts
+++ src/internal/core/sdk.ts
@@ -1,4 +1,5 @@
-import createCoreSdk from '@descope/core-js-sdk'
+import createCoreSdk, { type ExtendedResponse } from '@descope/core-js-sdk'
+import { Platform } from 'react-native'
 import { version } from '../../../package.json'
 
 export type Sdk = ReturnType<typeof createSdk>
@@ -6,8 +7,27 @@ export type SdkLogger = Parameters<typeof createCoreSdk>[0]['logger']
 export type SdkConfig = Parameters<typeof createCoreSdk>
 
 export const createSdk = (...config: SdkConfig) => {
-  config?.[0] && (config[0].baseHeaders = baseHeaders)
+  if (config[0]) {
+    // set base headers
+    config[0].baseHeaders = baseHeaders
+    // set parse cookie logic for iOS and Android
+    if (Platform.OS === 'ios' || Platform.OS === 'android') {
+      if (!config[0].hooks) config[0].hooks = {}
+      config[0].hooks.transformResponse = parseCookies
+    }
+  }
   return createCoreSdk(...config)
+}
+
+const parseCookies = async (mutableResponse: ExtendedResponse) => {
+  const resp = await mutableResponse.json()
+  if (mutableResponse.cookies.DS) {
+    resp.sessionJwt = mutableResponse.cookies.DS
+  }
+  if (mutableResponse.cookies.DSR) {
+    resp.refreshJwt = mutableResponse.cookies.DSR
+  }
+  return mutableResponse
 }
 
 const baseHeaders = {
