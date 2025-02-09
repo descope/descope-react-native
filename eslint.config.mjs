864b673691dd865618af848781db63b09b4d9998
Itai Hanski <itaihanski@gmail.com>
2025-02-09 20:51:47 +0200

    Update react native to 0.77.0 (#49)

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+import { fixupConfigRules } from '@eslint/compat'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+export default [
+  {
+    ignores: ['node_modules/', 'lib/'],
+  },
+  ...fixupConfigRules(compat.extends('@react-native-community', 'prettier')),
+]
