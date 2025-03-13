4ea88bcdf7265b7c84ac9fdd89097bf9843d760a
itaihanski <itaihanski@gmail.com>
2025-03-13 15:31:11 +0200

    Add deprecation notice

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ export { default as AuthProvider } from './components/AuthProvider'
 export { default as FlowView } from './components/FlowView'
 export { default as useDescope } from './hooks/useDescope'
 export { default as useSession } from './hooks/useSession'
+
+// deprecated
 export { default as useFlow } from './hooks/useFlow'
 
 export { getCurrentSessionToken, getCurrentRefreshToken, getCurrentUser } from './helpers'
 
-export type { DescopeSession, DescopeSessionManager, DescopeFlow, FlowOptions } from './types'
+export type { DescopeSession, DescopeSessionManager, DescopeFlow, FlowOptions, AndroidFlowOptions, iOSFlowOptions } from './types'
